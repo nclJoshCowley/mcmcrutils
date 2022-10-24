0487c7f5a625f7858286429e380ce46bc454084c
Josh Cowley <j.cowley1@ncl.ac.uk>
2022-10-24 16:07:51 +0100

    Package creation commit

---
+++ R/utils-imports.R
@@ -0,0 +1,17 @@
+#' Imports
+#'
+#' Imports from other packages used within this package.
+#'
+#' @name utils-imports
+#'
+#' @importFrom rlang .data
+#' @importFrom rlang %||%
+#' @importFrom magrittr %>%
+#'
+#' @keywords internal
+#'
+#' @section Links:
+#'   - [rlang::dot-data()], data pronoun.
+#'   - [rlang::op-null-default()], default value for NULL operator.
+#'   - [magrittr::%>%()], magrittr pipe.
+NULL
