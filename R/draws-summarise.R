227e52a7fd0bc32e1bc6b04958a11ddfc4d68e62
Josh Cowley <j.cowley1@ncl.ac.uk>
2022-10-25 21:21:16 +0100

    Added draws_summarise
    Also adds a ggdist dependency which may be reverted in a future version.

---
+++ R/draws-summarise.R
@@ -0,0 +1,23 @@
+#' Draws, Summarise
+#'
+#' Wrapper around [`point_interval`][ggdist::point_interval()] to calculate
+#'   point and interval summaries of draws.
+#'
+#' @template draws-method
+#' @param ... passed to [`point_interval()`][ggdist::point_interval()].
+#' @param .point function. Point estimate method (default changed to mean).
+#'
+#' @export
+draws_summarise <- function(draws, ..., .point = mean) {
+  args_default <-
+    list(
+      .data = dplyr::group_by(draws, .data$.chain, .data$.term),
+      .point = .point
+    )
+
+  args_updated <- utils::modifyList(args_default, rlang::list2(...))
+
+  out <- do.call(ggdist::point_interval, args_updated)
+  out[c(".width", ".point", ".interval")] <- NULL
+  return(out)
+}
