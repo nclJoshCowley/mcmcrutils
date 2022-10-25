45ce73105ca1a3b5708da77261c31d836bc5ba16
Josh Cowley <j.cowley1@ncl.ac.uk>
2022-10-25 21:10:36 +0100

    Simplified / reworked unnest term
    - Names are now forced
    - NA is given for missing dimensionality when multiple pars are given

--- R/draws-unnest-term.R
+++ R/draws-unnest-term.R
@@ -3,21 +3,35 @@
 #' Splits the `.term` column of a draws object into a data frame column.
 #'
 #' @template draws-method
-#' @param .names character. Dimension colnames, defaults to `dim_*`.
 #'
 #' @export
-draws_unnest_term <- function(draws, .names) {
-  t_indices <- as.data.frame(do.call(rbind, term::tindex(draws$.term)))
-  t_nms <- term::pars_terms(draws$.term)
-  nd <- NCOL(t_indices)
+draws_unnest_term <- function(draws) {
+  draws_per_par <- split(draws, term::pars_terms(draws$.term))
 
-  if (missing(.names)) .names <- sprintf("dim_%s", seq_len(nd))
+  unnested_draws <-
+    lapply(draws_per_par, function(.x) {
+      .x$.term <-
+        tibble::tibble(
+          par = term::pars(.x$.term),
+          term_dims_to_tibble(.x$.term)
+        )
 
-  if (length(.names) != nd) {
-    stop("Dimension mismatch (indices: ", nd, ", names: ", length(.names), ")")
-  }
+      return(.x)
+    })
 
-  draws$.term <- tibble::tibble(nm = t_nms, stats::setNames(t_indices, .names))
+  return(dplyr::bind_rows(unnested_draws))
+}
 
-  return(draws)
+
+#' Convert Term `dims` Field to Tibble
+#'
+#' @param x object of type [`term`][term::term()].
+#'
+#' @keywords internal
+#' @noRd
+term_dims_to_tibble <- function(x) {
+  tibble::as_tibble(
+    do.call(rbind, term::tindex(x)),
+    .name_repair = function(.nm) paste0("dim", seq_along(.nm))
+  )
 }
