09e8d395748ba41bf352f47db657ecbfbbaeb3da
Josh Cowley <j.cowley1@ncl.ac.uk>
2022-10-30 17:33:00 +0000

    Added build_drawsvis generic

--- R/drawsvis.R
+++ R/drawsvis.R
@@ -1,7 +1,10 @@
 #' Create `DrawsVis` tibble
 #'
-#' Nests and already grouped Draws-type tibble into a Drawsvis-type tibble
+#' Nests an already grouped Draws-type tibble into a Drawsvis-type tibble
 #'   with a list column of `ggplot2` objects.
+#'
+#' A generic, `build_drawsvis`, is supplied to be extended by modelling
+#'   packages to convert any object directly into a Drawsvis-type tibble.
 #'
 #' @template draws-method
 #' @template ggdraws-argument
@@ -23,4 +26,11 @@ drawsvis <- function(draws, .ggdraws, ...) {
     ) %>%
     dplyr::arrange(.data$.chain) %>%
     dplyr::relocate(.data$.chain, .before = 1)
+}
+
+
+#' @rdname drawsvis
+#' @export
+build_drawsvis <- function(x, ...) {
+  UseMethod("build_drawsvis")
 }
