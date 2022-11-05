6c14e75395f56f23b1c50df296be8a265f8fe8d2
Josh Cowley <j.cowley1@ncl.ac.uk>
2022-11-05 15:26:05 +0000

    Combined draws-vis methods into single R file

--- R/drawsvis-methods.R
+++ R/drawsvis-methods.R
@@ -1,4 +1,4 @@
-#' Drawsvis Methods
+#' Drawsvis, Add Subtitle(s)
 #'
 #' @template param-drawsvis
 #' @param ... Modifiers where position implies dimension.
@@ -41,4 +41,24 @@ drawsvis_add_term_subtitle <- function(dv, ...) {
   dv$.plot <- Map(`+`, dv$.plot, label_objects)
 
   return(dv)
+}
+
+
+#' Drawsvis to Patchwork
+#'
+#' Convert to a [`patchwork`][patchwork::patchwork-package] object.
+#'
+#' @template param-drawsvis
+#' @param ... extra arguments passed to [`wrap_plots`][patchwork::wrap_plots()].
+#' @param separate_chains logical. Input is split across `.chain` when `TRUE`.
+#'
+#' @return A [`patchwork`][patchwork::patchwork-package] object or a list of
+#'   such objects when `separate_chains` is `TRUE`.
+#'
+#' @export
+drawsvis_to_patchwork <- function(dv, ..., separate_chains = TRUE) {
+  if (isFALSE(separate_chains)) return(patchwork::wrap_plots(dv$.plot, ...))
+
+  dv_list <- draws_separate_chains(dv)
+  return(lapply(lapply(dv_list, `[[`, ".plot"), patchwork::wrap_plots, ...))
 }
